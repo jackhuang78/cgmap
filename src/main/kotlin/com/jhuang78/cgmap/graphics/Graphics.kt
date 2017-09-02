3d79ac413cfef133066e5894dc9653074d9f71be
Jack Huang <jack.huang78@gmail.com>
2017-09-01 22:21:49 -0700

    InfoFileReader with unit tests

--- src/main/kotlin/com/jhuang78/cgmap/graphics/Graphics.kt
+++ src/main/kotlin/com/jhuang78/cgmap/graphics/Graphics.kt
@@ -1,36 +1,84 @@
 package com.jhuang78.cgmap.graphics
 
+import com.google.common.base.Preconditions.checkElementIndex
+import com.google.common.base.Preconditions.checkState
+import com.google.common.base.Preconditions.checkArgument
+import com.google.common.primitives.Longs
+import java.nio.ByteOrder
+import java.nio.channels.FileChannel
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+
 data class Info(
-        val graphicNo : Int = 0,
-        val address : Int = 0,
-        val dataLength : Int = 0,
-        val offsetX : Int = 0,
-        val offsetY : Int = 0,
-        val imageWidth : Int = 0,
+        val graphicNo: Int = 0,
+        val address: Int = 0,
+        val dataLength: Int = 0,
+        val offsetX: Int = 0,
+        val offsetY: Int = 0,
+        val imageWidth: Int = 0,
         val imageHeight: Int = 0,
         val occupyEast: Int = 0,
         val occupySouth: Int = 0,
         val mark: Int = 0,
         val unknown: Long = 0L,
         val mapNo: Int = 0
-)
+) {
+    public fun validate(): Info {
+        checkState(graphicNo >= 0, "Expect non-negative graphicNo, but got ${graphicNo}.")
+        checkState(address >= 0, "Expect non-negative address, but got ${address}.")
+        checkState(dataLength >= 0, "Expect non-negative dataLength, but got ${dataLength}.")
+        checkState(imageWidth >= 0, "Expect non-negative imageWidth, but got ${imageWidth}.")
+        checkState(imageHeight >= 0, "Expect non-negative imageHeight, but got ${imageHeight}.")
+        checkState(occupyEast >= 0, "Expect non-negative occupyEast, but got ${occupyEast}.")
+        checkState(occupySouth >= 0, "Expect non-negative occupySouth, but got ${occupySouth}.")
+        checkState(mark == 0 || mark == 1, "Expect mark to be either 0 or 1, but got ${mark}.")
+        checkState(mapNo >= 0, "Expect non-negative mapNo, but got ${mapNo}.")
+        return this
+    }
+}
 
-fun Info.validate() : Info {
-    fun checkNonNegative(name : String, value : Int) {
-        check(value >= 0) {"Expect non-negative ${name}, but got ${value}"}
+const val INFO_ENTRY_SIZE = 40L
+
+class InfoFileReader(val path: Path) {
+    val fc: FileChannel
+    val size: Int
+
+    init {
+        checkArgument(path.toFile().isFile(), "Path ${path} does not point to a file.")
+        fc = FileChannel.open(path, StandardOpenOption.READ);
+        size = (fc.size() / INFO_ENTRY_SIZE).toInt()
     }
 
-    check(graphicNo >= 0) {"Expect non-negative graphicNo, but got ${graphicNo}."}
-    check(address >= 0) {"Expect non-negative address, but got ${address}."}
-    check(dataLength >= 0) {"Expect non-negative dataLength, but got ${dataLength}."}
-    check(offsetX >= 0) {"Expect non-negative offsetX, but got ${offsetX}."}
-    check(offsetY >= 0) {"Expect non-negative offsetY, but got ${offsetY}."}
-    check(imageWidth >= 0) {"Expect non-negative imageWidth, but got ${imageWidth}."}
-    check(imageHeight >= 0) {"Expect non-negative imageHeight, but got ${imageHeight}."}
-    check(occupyEast >= 0) {"Expect non-negative occupyEast, but got ${occupyEast}."}
-    check(occupySouth >= 0) {"Expect non-negative occupySouth, but got ${occupySouth}."}
-    check(mark == 0 || mark == 1) {"Expect mark to be either 0 or 1, but got ${mark}."}
-    check(mapNo >= 0) {"Expect non-negative mapNo, but got ${mapNo}."}
+    public fun read(entryNo: Int): Info {
+        checkElementIndex(entryNo, size)
 
-    return this
+        val buffer = fc
+                .map(FileChannel.MapMode.READ_ONLY, entryNo * INFO_ENTRY_SIZE, INFO_ENTRY_SIZE)
+                .order(ByteOrder.LITTLE_ENDIAN)
+
+        return Info(
+                graphicNo = buffer.getInt(),
+                address = buffer.getInt(),
+                dataLength = buffer.getInt(),
+                offsetX = buffer.getInt(),
+                offsetY = buffer.getInt(),
+                imageWidth = buffer.getInt(),
+                imageHeight = buffer.getInt(),
+                occupyEast = buffer.get().toInt(),
+                occupySouth = buffer.get().toInt(),
+                mark = buffer.get().toInt(),
+                unknown = Longs.fromBytes(
+                        buffer.get(),
+                        buffer.get(),
+                        buffer.get(),
+                        buffer.get(),
+                        buffer.get(),
+                        0.toByte(),
+                        0.toByte(),
+                        0.toByte()
+                ),
+                mapNo = buffer.getInt()
+        ).validate()
+    }
+
 }
