40f8ebddd041a25c5199aef3fbc2a3db2f8e2823
Jack Huang <jack.huang78@gmail.com>
2018-06-14 04:28:50 -0700

    Test case refactoring

--- src/test/kotlin/com/jhuang78/cgmap/io/GraphicDataDecoderTest.kt
+++ src/test/kotlin/com/jhuang78/cgmap/io/GraphicDataDecoderTest.kt
@@ -13,91 +13,96 @@ class GraphicDataDecoderTest : Spek({
 		val r1 = Random(0)
 		val r2 = Random(0)
 
+		data class TestCase(
+				val name: String,
+				val data: List<Int?>,
+				val expected: List<Int?>
+		)
+
 		listOf(
-				arrayOf("01", 0, 2,
-						listOf(0x01, 0x23),
-						listOf(0x23)),
-				arrayOf("02", 0, 3,
-						listOf(0x02, 0x34, 0x56),
-						listOf(0x34, 0x56)),
-				arrayOf("0F", 0, 1 + 0x0F,
-						listOf(0x0F).appendRandom(0x0F, r1),
-						listOf<Int>().appendRandom(0x0F, r2)),
-				arrayOf("10 01", 0, 3,
-						listOf(0x10, 0x01, 0x23),
-						listOf(0x23)),
-				arrayOf("11 12", 0, 2 + 0x123,
-						listOf(0x11, 0x23).appendRandom(0x123, r1),
-						listOf<Int>().appendRandom(0x123, r2)),
-				arrayOf("1F FF", 0, 2 + 0xFFF,
-						listOf(0x1F, 0xFF).appendRandom(0xFFF, r1),
-						listOf<Int>().appendRandom(0xFFF, r2)),
-				arrayOf("20 00 01", 0, 4,
-						listOf(0x20, 0x00, 0x01, 0x23),
-						listOf(0x23)),
-				arrayOf("21 12 34", 0, 3 + 0x12345,
-						listOf(0x21, 0x23, 0x45).appendRandom(0x12345, r1),
-						listOf<Int>().appendRandom(0x12345, r2)),
-				arrayOf("2F FF FF", 0, 3 + 0xFFFFF,
-						listOf(0x2F, 0xFF, 0xFF).appendRandom(0xFFFFF, r1),
-						listOf<Int>().appendRandom(0xFFFFF, r2)),
-				arrayOf("81", 0, 2,
-						listOf(0x81, 0x23),
-						listOf(0x23)),
-				arrayOf("82", 0, 2,
-						listOf(0x82, 0x23),
-						listOf(0x23, 0x23)),
-				arrayOf("8F", 0, 2,
-						listOf(0x8F, 0x23),
-						listOf<Int>().appendRepeated(0xF, 0x23)),
-				arrayOf("90 01", 0, 3,
-						listOf(0x90, 0x23, 0x01),
-						listOf(0x23)),
-				arrayOf("90 12", 0, 3,
-						listOf(0x91, 0x45, 0x23),
-						listOf<Int>().appendRepeated(0x123, 0x45)),
-				arrayOf("9F FF", 0, 3,
-						listOf(0x9F, 0x45, 0xFF),
-						listOf<Int>().appendRepeated(0xFFF, 0x45)),
-				arrayOf("A0 00 01", 0, 4,
-						listOf(0xA0, 0x23, 0x00, 0x01),
-						listOf(0x23)),
-				arrayOf("A0 12 34", 0, 4,
-						listOf(0xA1, 0x67, 0x23, 0x45),
-						listOf<Int>().appendRepeated(0x12345, 0x67)),
-				arrayOf("AF FF FF", 0, 4,
-						listOf(0xAF, 0x67, 0xFF, 0xFF),
-						listOf<Int>().appendRepeated(0xFFFFF, 0x67)),
-				arrayOf("C1", 0, 1,
-						listOf(0xC1),
-						listOf(null)),
-				arrayOf("C2", 0, 1,
-						listOf(0xC2),
-						listOf(null, null)),
-				arrayOf("CF", 0, 1,
-						listOf(0xCF),
-						listOf<Int>().appendRepeated(0xF, null)),
-				arrayOf("D0 01", 0, 2,
-						listOf(0xD0, 0x01),
-						listOf(null)),
-				arrayOf("D1 23", 0, 2,
-						listOf(0xD1, 0x23),
-						listOf<Int>().appendRepeated(0x123, null)),
-				arrayOf("DF FF", 0, 2,
-						listOf(0xDF, 0xFF),
-						listOf<Int>().appendRepeated(0xFFF, null)),
-				arrayOf("E0 00 01", 0, 3,
-						listOf(0xE0, 0x00, 0x01),
-						listOf(null)),
-				arrayOf("E1 23 45", 0, 3,
-						listOf(0xE1, 0x23, 0x45),
-						listOf<Int>().appendRepeated(0x12345, null)),
-				arrayOf("EF FF FF", 0, 3,
-						listOf(0xEF, 0xFF, 0xFF),
-						listOf<Int>().appendRepeated(0xFFFFF, null)),
-				arrayOf("Mixed", 0,
-						1 + 0x1 + 2 + 0x202 + 3 + 0x30303 + 2 + 3 + 4 + 1 + 2 + 3,
-						listOf<Int>()
+				TestCase(name = "01",
+						data = listOf(0x01, 0x23),
+						expected = listOf(0x23)),
+				TestCase(name = "02",
+						data = listOf(0x02, 0x34, 0x56),
+						expected = listOf(0x34, 0x56)),
+				TestCase(name = "0F",
+						data = listOf(0x0F).appendRandom(0x0F, r1),
+						expected = listOf<Int>().appendRandom(0x0F, r2)),
+				TestCase(name = "10 01",
+						data = listOf(0x10, 0x01, 0x23),
+						expected = listOf(0x23)),
+				TestCase(name = "11 12",
+						data = listOf(0x11, 0x23).appendRandom(0x123, r1),
+						expected = listOf<Int>().appendRandom(0x123, r2)),
+				TestCase(name = "1F FF",
+						data = listOf(0x1F, 0xFF).appendRandom(0xFFF, r1),
+						expected = listOf<Int>().appendRandom(0xFFF, r2)),
+				TestCase(name = "20 00 01",
+						data = listOf(0x20, 0x00, 0x01, 0x23),
+						expected = listOf(0x23)),
+				TestCase(name = "21 12 34",
+						data = listOf(0x21, 0x23, 0x45).appendRandom(0x12345, r1),
+						expected = listOf<Int>().appendRandom(0x12345, r2)),
+				TestCase(name = "2F FF FF",
+						data = listOf(0x2F, 0xFF, 0xFF).appendRandom(0xFFFFF, r1),
+						expected = listOf<Int>().appendRandom(0xFFFFF, r2)),
+				TestCase(name = "81",
+						data = listOf(0x81, 0x23),
+						expected = listOf(0x23)),
+				TestCase(name = "82",
+						data = listOf(0x82, 0x23),
+						expected = listOf(0x23, 0x23)),
+				TestCase(name = "8F",
+						data = listOf(0x8F, 0x23),
+						expected = listOf<Int>().appendRepeated(0xF, 0x23)),
+				TestCase(name = "90 01",
+						data = listOf(0x90, 0x23, 0x01),
+						expected = listOf(0x23)),
+				TestCase(name = "90 12",
+						data = listOf(0x91, 0x45, 0x23),
+						expected = listOf<Int>().appendRepeated(0x123, 0x45)),
+				TestCase(name = "9F FF",
+						data = listOf(0x9F, 0x45, 0xFF),
+						expected = listOf<Int>().appendRepeated(0xFFF, 0x45)),
+				TestCase(name = "A0 00 01",
+						data = listOf(0xA0, 0x23, 0x00, 0x01),
+						expected = listOf(0x23)),
+				TestCase(name = "A0 12 34",
+						data = listOf(0xA1, 0x67, 0x23, 0x45),
+						expected = listOf<Int>().appendRepeated(0x12345, 0x67)),
+				TestCase(name = "AF FF FF",
+						data = listOf(0xAF, 0x67, 0xFF, 0xFF),
+						expected = listOf<Int>().appendRepeated(0xFFFFF, 0x67)),
+				TestCase(name = "C1",
+						data = listOf(0xC1),
+						expected = listOf(null)),
+				TestCase(name = "C2",
+						data = listOf(0xC2),
+						expected = listOf(null, null)),
+				TestCase(name = "CF",
+						data = listOf(0xCF),
+						expected = listOf<Int>().appendRepeated(0xF, null)),
+				TestCase(name = "D0 01",
+						data = listOf(0xD0, 0x01),
+						expected = listOf(null)),
+				TestCase(name = "D1 23",
+						data = listOf(0xD1, 0x23),
+						expected = listOf<Int>().appendRepeated(0x123, null)),
+				TestCase(name = "DF FF",
+						data = listOf(0xDF, 0xFF),
+						expected = listOf<Int>().appendRepeated(0xFFF, null)),
+				TestCase(name = "E0 00 01",
+						data = listOf(0xE0, 0x00, 0x01),
+						expected = listOf(null)),
+				TestCase(name = "E1 23 45",
+						data = listOf(0xE1, 0x23, 0x45),
+						expected = listOf<Int>().appendRepeated(0x12345, null)),
+				TestCase(name = "EF FF FF",
+						data = listOf(0xEF, 0xFF, 0xFF),
+						expected = listOf<Int>().appendRepeated(0xFFFFF, null)),
+				TestCase(name = "Mixed",
+						data = listOf<Int>()
 								.append(0x01).appendRandom(0x1, r1)
 								.append(0x12, 0x02).appendRandom(0x202, r1)
 								.append(0x23, 0x03, 0x03).appendRandom(0x30303, r1)
@@ -107,7 +112,7 @@ class GraphicDataDecoderTest : Spek({
 								.append(0xCD)
 								.append(0xDE, 0x0E)
 								.append(0xEF, 0x0F, 0x10),
-						listOf<Int>()
+						expected = listOf<Int>()
 								.appendRandom(0x1, r2)
 								.appendRandom(0x202, r2)
 								.appendRandom(0x30303, r2)
@@ -117,20 +122,21 @@ class GraphicDataDecoderTest : Spek({
 								.appendRepeated(0xD, null)
 								.appendRepeated(0xE0E, null)
 								.appendRepeated(0xF0F10, null))
-		).forEach { (name, start, length, data, output) ->
+
+		).forEach { (name, data, expected) ->
 
 			describe("case ${name}") {
-				val decoder = GraphicDataDecoder((data as List<Int>).toByteBuffer())
+				val decoder = GraphicDataDecoder(data.toByteBuffer())
 
 				it("should decode data correctly") {
-					val expected = (output as List<Int>).iterator()
+					val expectedIterator = expected.iterator()
 					val actual = decoder.iterator()
 
 					var iteration = 0
-					while (expected.hasNext()) {
+					while (expectedIterator.hasNext()) {
 						assertWithMessage("${iteration}").that(actual.hasNext()).isTrue()
 						assertWithMessage("${iteration}").that(actual.next()).isEqualTo(
-								expected.next())
+								expectedIterator.next())
 						iteration++
 					}
 					assertThat(actual.hasNext()).isFalse()
@@ -140,8 +146,8 @@ class GraphicDataDecoderTest : Spek({
 	}
 })
 
-fun List<Int>.toByteBuffer() = ByteBuffer.wrap(
-		this.map { it.toByte() }.toByteArray()
+fun List<Int?>.toByteBuffer() = ByteBuffer.wrap(
+		this.map { it!!.toByte() }.toByteArray()
 )
 
 fun List<Int?>.appendRandom(n: Int, r: Random): List<Int?> {
