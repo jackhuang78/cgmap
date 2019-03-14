70393bc30f46ead9b1a477fa416308af9f05093d
Jack Huang <jack.huang78@gmail.com>
2019-03-14 00:28:35 -0700

    Refactor and simplify GraphicFileReader

--- src/main/kotlin/com/jhuangyc/cgmap/io/GraphicFileReader.kt
+++ src/main/kotlin/com/jhuangyc/cgmap/io/GraphicFileReader.kt
@@ -1,6 +1,5 @@
 package com.jhuangyc.cgmap.io
 
-import com.google.common.base.Preconditions.checkElementIndex
 import com.jhuangyc.cgmap.entity.Graphic
 import com.jhuangyc.cgmap.entity.Graphic.Version.ENCODED
 import com.jhuangyc.cgmap.entity.Graphic.Version.RAW
@@ -10,7 +9,7 @@ import java.nio.ByteOrder.LITTLE_ENDIAN
 import java.nio.channels.FileChannel
 import java.nio.channels.FileChannel.MapMode.READ_ONLY
 import java.nio.file.Path
-import java.nio.file.StandardOpenOption
+import java.nio.file.StandardOpenOption.READ
 
 
 /**
@@ -18,53 +17,26 @@ import java.nio.file.StandardOpenOption
  */
 class GraphicFileReader(val path: Path) : AutoCloseable {
 	companion object {
-		/**
-		 * Size of the header part of the entry (i.e. everything except the actual
-		 * data).
-		 */
-		private val GRAPHIC_ENTRY_HEADER_SIZE = 16
+		private val HEADER_SIZE = 16
 
-		/**
-		 * The magic value to mapMarker the beginning of each Graphic entry.
-		 * The value is "RD" in bytes.
-		 * Ints.fromBytes(0, 0, 'D'.toByte(), 'R'.toByte())
-		 */
-		private val GRAPHIC_ENTRY_MAGIC = 0x4452
+		// Each entry begins with this two-byte magic value: 'RD'
+		private val MAGIC = 0x4452
 
-		/**
-		 * Validates that all values in this Graphic are valid.
-		 */
-		fun Graphic.validate() {
-			check(magic == GRAPHIC_ENTRY_MAGIC) {
-				"Expect magic to be ${GRAPHIC_ENTRY_MAGIC} (\"RD\"), but got ${magic}"
-			}
-			check(width > 0) { "Expect width to be positive, but got ${width}" };
-			check(height > 0) { "Expect height to be positive, but got ${height}" };
-			check(dataLength == GRAPHIC_ENTRY_HEADER_SIZE + data.capacity()) {
-				"Expect data length to be ${GRAPHIC_ENTRY_HEADER_SIZE}(header)"
-				+ " + ${data.capacity()}(data)"
-				+ " = ${GRAPHIC_ENTRY_HEADER_SIZE + data.capacity()},"
-				+ " but got ${dataLength}."
-			}
-
-			//TODO 7476, (?)
-		}
+		//TODO 7476, (?)
 	}
 
 	private val logger = KotlinLogging.logger {}
 
-	private val fileChannel = FileChannel.open(path, StandardOpenOption.READ)
+	private val fileChannel by lazy {
+		FileChannel.open(path, READ)
+	}
 
-	/**
-	 * Reads an Graphic from file.
-	 */
 	fun read(position: Int, size: Int): Graphic {
-		checkElementIndex(position, fileChannel.size().toInt())
-
 		logger.debug("Reading ${size} bytes from ${path} at position ${position}")
 
-		val buffer = fileChannel.map(READ_ONLY, position.toLong(),
-				size.toLong()).order(LITTLE_ENDIAN);
+		val buffer = fileChannel
+				.map(READ_ONLY, position.toLong(), size.toLong())
+				.order(LITTLE_ENDIAN)
 
 		val graphic = Graphic(
 				magic = buffer.getShort().toUint(),
@@ -72,13 +44,26 @@ class GraphicFileReader(val path: Path) : AutoCloseable {
 				unknown = buffer.get().toUint(),
 				width = buffer.getInt(),
 				height = buffer.getInt(),
-				dataLength = buffer.getInt(), // Read the rest of hte buffer
+				dataLength = buffer.getInt(), // Read the rest of the buffer
 				data = buffer.slice().asReadOnlyBuffer())
 
-		try {
-			graphic.validate()
-		} catch (e: IllegalStateException) {
-			logger.error(e) { "The read Graphic is probably corrupted." }
+		check(graphic.magic == MAGIC) {
+			"Expect magic to be ${MAGIC} (\"RD\"), but got ${graphic.magic}"
+		}
+		check(graphic.width > 0) {
+			"Expect width to be positive, but got ${graphic.width}"
+		}
+		check(graphic.height > 0) {
+			"Expect height to be positive, but got ${graphic.height}"
+		}
+		check(graphic.dataLength == (HEADER_SIZE + graphic.data.capacity())) {
+			"""
+				Expect data length to be
+				${HEADER_SIZE}(header)
+				+ ${graphic.data.capacity()}(data)
+				= ${HEADER_SIZE + graphic.data.capacity()},
+				but got $graphic.{dataLength}.
+			""".trimIndent()
 		}
 
 		return graphic
