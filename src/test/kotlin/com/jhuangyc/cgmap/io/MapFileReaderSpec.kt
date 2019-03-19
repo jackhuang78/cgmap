acfd02ee8a001312b1a543baf4a4cd10e83455cf
Jack Huang <jack.huang78@gmail.com>
2019-03-19 01:28:57 -0700

    Refactor test for MapFileReader

--- src/test/kotlin/com/jhuangyc/cgmap/io/MapFileReaderSpec.kt
+++ src/test/kotlin/com/jhuangyc/cgmap/io/MapFileReaderSpec.kt
@@ -3,24 +3,25 @@ package com.jhuangyc.cgmap.io
 import com.google.common.truth.Truth.assertThat
 import com.jhuangyc.cgmap.util.fromResources
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 import java.nio.file.Paths
 
-class MapFileReaderTest : Spek({
+object MapFileReaderSpec : Spek({
 
-	describe("readMapFile()") {
-		given("an valid file") {
-			val map = MapFileReader(
-					Paths.get("map", "0", "100.dat").fromResources()).read()
+	given("a reader") {
+		val file = Paths.get("map", "0", "100.dat").fromResources()
+		val reader = MapFileReader(file)
+
+		on("read()") {
+			val map = reader.read()
+
 			it("should read header correctly") {
 				assertThat(map.eastLength).isEqualTo(0x0348)
 				assertThat(map.southLength).isEqualTo(0x0262)
+
 				// TODO: check the rest of the header
-			}
-			it("should past validation") {
-				map.validate()
 			}
 		}
 	}
