ed1db808af5a5102ffe2d5125eb7708567ffdacc
Jack Huang <jack.huang78@gmail.com>
2019-08-12 02:48:06 -0700

    Migrate to spek2

--- src/test/kotlin/com/jhuangyc/cgmap/io/GraphicInfoFileReaderSpec.kt
+++ src/test/kotlin/com/jhuangyc/cgmap/io/GraphicInfoFileReaderSpec.kt
@@ -1,94 +1,85 @@
 package com.jhuangyc.cgmap.io
 
-import com.google.common.truth.Truth.assertThat
 import com.jhuangyc.cgmap.entity.GraphicInfo
 import com.jhuangyc.cgmap.util.fromResources
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.given
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
+import org.spekframework.spek2.Spek
+import strikt.api.expectCatching
+import strikt.api.expectThat
+import strikt.assertions.failed
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
 import java.io.IOException
 import java.nio.file.Paths
-import kotlin.test.assertFails
-
 
 object GraphicInfoFileReaderSpec : Spek({
 
-	given("a reader with file containing 2 entries") {
+	group("Given a file containing 2 entries") {
 		val validFile = Paths.get("GraphicInfo_66_small.bin").fromResources()
 		val reader = GraphicInfoFileReader(validFile)
 
-		on("numEntries()") {
-			val numEntries = reader.numEntries
-
-			it("should be 2") {
-				assertThat(numEntries).isEqualTo(2)
-			}
+		test("Should know the correct number of entries") {
+			expectThat(reader.numEntries).isEqualTo(2)
 		}
 
-		on("read(0)") {
+		test("Should read the first entry") {
 			val info = reader.read(0)
 
-			it("should read 1st entry correctly") {
-				assertThat(info.graphicNo).isEqualTo(0)
-				assertThat(info.address).isEqualTo(0x0000)
-				assertThat(info.dataLength).isEqualTo(0x01a8)
-				assertThat(info.offsetX).isEqualTo(-32)
-				assertThat(info.offsetY).isEqualTo(-24)
-				assertThat(info.imageWidth).isEqualTo(64)
-				assertThat(info.imageHeight).isEqualTo(47)
-				assertThat(info.occupyEast).isEqualTo(1)
-				assertThat(info.occupySouth).isEqualTo(1)
-				assertThat(info.mapMarker).isEqualTo(
-						GraphicInfo.MapMarker.FLOOR)
-				assertThat(info.graphicId).isEqualTo(0x03e7)
+			expectThat(info) {
+				get { graphicNo }.isEqualTo(0)
+				get { address }.isEqualTo(0x0000)
+				get { dataLength }.isEqualTo(0x01a8)
+				get { offsetX }.isEqualTo(-32)
+				get { offsetY }.isEqualTo(-24)
+				get { imageWidth }.isEqualTo(64)
+				get { imageHeight }.isEqualTo(47)
+				get { occupyEast }.isEqualTo(1)
+				get { occupySouth }.isEqualTo(1)
+				get { mapMarker }.isEqualTo(GraphicInfo.MapMarker.FLOOR)
+				get { graphicId }.isEqualTo(0x03e7)
 			}
 		}
 
-		on("read(1)") {
+		test("Should read the second entry") {
 			val info = reader.read(1)
 
-			it("should read 2nd entry correctly") {
-				assertThat(info.graphicNo).isEqualTo(1)
-				assertThat(info.address).isEqualTo(0x01a8)
-				assertThat(info.dataLength).isEqualTo(0x0012)
-				assertThat(info.offsetX).isEqualTo(-32)
-				assertThat(info.offsetY).isEqualTo(-24)
-				assertThat(info.imageWidth).isEqualTo(64)
-				assertThat(info.imageHeight).isEqualTo(48)
-				assertThat(info.occupyEast).isEqualTo(1)
-				assertThat(info.occupySouth).isEqualTo(1)
-				assertThat(info.mapMarker).isEqualTo(
-						GraphicInfo.MapMarker.OBSTACLE)
-				assertThat(info.graphicId).isEqualTo(0x0012)
+			expectThat(info) {
+				get { graphicNo }.isEqualTo(1)
+				get { address }.isEqualTo(0x01a8)
+				get { dataLength }.isEqualTo(0x0012)
+				get { offsetX }.isEqualTo(-32)
+				get { offsetY }.isEqualTo(-24)
+				get { imageWidth }.isEqualTo(64)
+				get { imageHeight }.isEqualTo(48)
+				get { occupyEast }.isEqualTo(1)
+				get { occupySouth }.isEqualTo(1)
+				get { mapMarker }.isEqualTo(GraphicInfo.MapMarker.OBSTACLE)
+				get { graphicId }.isEqualTo(0x0012)
 			}
 		}
 
-		on("read(2)") {
-			val thrown = assertFails { reader.read(2) }
-
-			it("should fail") {
-				assertThat(thrown).isInstanceOf(IOException::class.java)
-			}
+		test("Should fail to read the third entry") {
+			expectCatching { reader.read(2) }
+				.failed()
+				.isA<IOException>()
 		}
+
 	}
 
-	listOf(
-			arrayOf("invalid file", "random.bin", IllegalStateException::class.java),
-			arrayOf("directory", ".", IOException::class.java)
-	).forEach { (case, inputFile, expectedError) ->
+	test("Should fail to read an invalid input file") {
+		val invalidFile = Paths.get("random.bin").fromResources()
+		val reader = GraphicInfoFileReader(invalidFile)
 
-		given("a reader with ${case} as input file") {
-			val invalidFile = Paths.get(inputFile as String).fromResources()
-			val reader = GraphicInfoFileReader(invalidFile)
+		expectCatching { reader.read(0) }
+			.failed()
+			.isA<IllegalStateException>()
+	}
 
-			on("read()") {
-				val thrown = assertFails { reader.read(0) }
+	test("Should fail to read a directory") {
+		val invalidFile = Paths.get(".").fromResources()
+		val reader = GraphicInfoFileReader(invalidFile)
 
-				it("should fail") {
-					assertThat(thrown).isInstanceOf(expectedError as Class<*>)
-				}
-			}
-		}
+		expectCatching { reader.read(0) }
+			.failed()
+			.isA<IOException>()
 	}
 })
--- src/test/kotlin/com/jhuangyc/cgmap/io/PaletFileReaderSpec.kt
+++ src/test/kotlin/com/jhuangyc/cgmap/io/PaletFileReaderSpec.kt
@@ -1,51 +1,48 @@
 package com.jhuangyc.cgmap.io
 
-import com.google.common.truth.Truth.assertThat
 import com.jhuangyc.cgmap.util.fromResources
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.given
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
+import org.spekframework.spek2.Spek
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 import java.awt.Color
 import java.nio.file.Paths
 
 object PaletFileReaderSpec : Spek({
 
-	given("a reader") {
+	group("Given a palet file") {
 		val paletFile = Paths.get("palet", "palet_00.cgp").fromResources()
 		val reader = PaletFileReader(paletFile)
 
-		on("read()") {
-			val palet = reader.read()
+		listOf(
+			arrayOf(0, Color(0x00, 0x00, 0x00)),
+			arrayOf(15, Color(0x28, 0xE1, 0x28)),
+			arrayOf(240, Color(0x96, 0xC3, 0xF5)),
+			arrayOf(255, Color(0xFF, 0xFF, 0xFF))
+		).forEach { (colorIdx, expectedColor) ->
 
-			listOf(
-					arrayOf(0, Color(0x00, 0x00, 0x00)),
-					arrayOf(15, Color(0x28, 0xE1, 0x28)),
-					arrayOf(240, Color(0x96, 0xC3, 0xF5)),
-					arrayOf(255, Color(0xFF, 0xFF, 0xFF))
-			).forEach { (colorIdx, expectedColor) ->
+			test("Should read base color $colorIdx") {
+				val palet = reader.read()
 
-				it("should read base color ${colorIdx}") {
-					assertThat(palet.colors[colorIdx as Int])
-							.isEqualTo(expectedColor as Color)
-				}
+				expectThat(palet.colors[colorIdx as Int]).isEqualTo(
+					expectedColor as Color)
 			}
+		}
 
-			listOf(
-					arrayOf(16, Color(0xA4, 0xF7, 0x85)),
-					arrayOf(17, Color(0x96, 0xE2, 0x7A)),
-					arrayOf(32, Color(0xB9, 0x52, 0x41)),
-					arrayOf(64, Color(0xBE, 0x8A, 0xEC)),
-					arrayOf(128, Color(0xA0, 0xC8, 0xF6)),
-					arrayOf(239, Color(0x92, 0x53, 0x4D))
-			).forEach { (colorIdx, expectedColor) ->
+		listOf(
+			arrayOf(16, Color(0xA4, 0xF7, 0x85)),
+			arrayOf(17, Color(0x96, 0xE2, 0x7A)),
+			arrayOf(32, Color(0xB9, 0x52, 0x41)),
+			arrayOf(64, Color(0xBE, 0x8A, 0xEC)),
+			arrayOf(128, Color(0xA0, 0xC8, 0xF6)),
+			arrayOf(239, Color(0x92, 0x53, 0x4D))
+		).forEach { (colorIdx, expectedColor) ->
 
-				it("should read custom color ${colorIdx}") {
-					assertThat(palet.colors[colorIdx as Int])
-							.isEqualTo(expectedColor as Color)
-				}
+			test("Should read custom color $colorIdx") {
+				val palet = reader.read()
+
+				expectThat(palet.colors[colorIdx as Int])
+					.isEqualTo(expectedColor as Color)
 			}
-
 		}
 	}
 })
